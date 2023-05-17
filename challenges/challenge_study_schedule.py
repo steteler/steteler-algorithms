c5b95dc887824953e8cdf8d68f7b970c9656f338
kikuti <joaovictorkikuti@gmail.com>
2023-05-17 20:46:25 -0300

    feat: terminado requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        students_quantity = 0
+        for study_entry, study_exit in permanence_period:
+            if study_entry <= target_time <= study_exit:
+                students_quantity += 1
+        return students_quantity
+    except (TypeError):
+        return None
