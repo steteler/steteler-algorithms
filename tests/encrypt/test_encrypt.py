cf21c6c4a07c9a5ce18e8398ec09f17a697155a9
kikuti <joaovictorkikuti@gmail.com>
2023-05-17 23:07:40 -0300

    feat: terminado requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "kikuti"
+
+    with pytest.raises(TypeError):
+        encrypt_message(2, "deve retornar erro")
+
+    assert encrypt_message(message, -1) == "itukik"
+
+    assert encrypt_message(message, 1) == "k_ituki"
+
+    assert encrypt_message(message, 2) == "ituk_ik"
