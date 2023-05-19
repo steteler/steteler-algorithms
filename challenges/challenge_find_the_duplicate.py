30325940225a398bc5e6209528a8f416ed143097
kikuti <joaovictorkikuti@gmail.com>
2023-05-18 21:46:13 -0300

    feat: terminado requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(nums) <= 1):
+        return False
+
+    nums.sort()
+
+    prev_num = nums[0]
+    for num in nums[1:]:
+        if not isinstance(num, int) or num <= 0:
+            return False
+        elif num == prev_num:
+            return num
+        prev_num = num
+    return False
